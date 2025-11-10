45014d12e77f0697cc6df8c99937e4a97a521c03
Nihar1604 <niharthalkar68@gmail.com>
2025-11-10 18:58:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5500',
   'http://localhost:3000',
-  'null'
+  'null',
+  'https://artisian-connect-frontend-git-main-nihar1604s-projects.vercel.app'
 ];
 
 const corsOptions = {
